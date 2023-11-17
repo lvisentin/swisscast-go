280c0ceb34d78bfa084d8427918f8e81816d4426
lvisentin <lvise.batista@gmail.com>
2023-11-17 16:52:52 -0300

    feat: cors and get rss feed

---
+++ controllers/feedController.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mmcdole/gofeed"
+)
+
+type RSSFeedRequestParams struct {
+	Username string `json:"username"`
+	Pass  string `json:"pass"`
+	Url   string `json:"url"`
+}
+
+func GetRssFeed(c *gin.Context) {
+	var rssFeedParams RSSFeedRequestParams
+	c.BindJSON(&rssFeedParams)
+
+	fp := gofeed.NewParser()
+	fp.AuthConfig = &gofeed.Auth{
+		Username: rssFeedParams.Username,
+		Password: rssFeedParams.Pass,
+	  }
+	feed, _ := fp.ParseURL(rssFeedParams.Url)
+	c.JSON(http.StatusOK, gin.H{"feed":feed})
+}
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"swisscast-go/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
+
+
+
+func main() {
+	router := gin.Default()
+	router.Use(CORSMiddleware())
+
+	router.POST("/feed", controllers.GetRssFeed)
+	router.Run("localhost:8080")
+}
